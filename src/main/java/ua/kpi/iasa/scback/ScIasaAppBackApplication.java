583df5ed729a1bc3e8f46610eccc87abba96b2a4
PrometheusUA <prometheusua@i.ua>
2021-12-12 16:01:56 +0200

    First commit

---
+++ src/main/java/ua/kpi/iasa/scback/ScIasaAppBackApplication.java
@@ -0,0 +1,13 @@
+package ua.kpi.iasa.scback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScIasaAppBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScIasaAppBackApplication.class, args);
+    }
+
+}
